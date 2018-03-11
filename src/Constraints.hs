bd68824728744693e6fc6a842b8e646f78029ed4
benstrac <35542776+benstrac@users.noreply.github.com>
2018-03-10 18:58:59 -0700

    Updates Constraints

--- src/Constraints.hs
+++ src/Constraints.hs
@@ -8,50 +8,44 @@ import Data.List
 
 --x = branchAndBound x y
 
-
-meetsHardConstr :: [(Char,Char)] -> [(Char,Char)] -> [(Char,Char)] -> [Char] -> Bool
-
+meetsHardConstr :: [(Int,Char)] -> [(Int,Char)] -> [(Char,Char)] -> [Char] -> Bool
 meetsHardConstr fpa fm tnt state = iterState fpa fm state && meetsTnt tnt state
 
--- takes the fpa, fm, and the state
-iterState :: [(Char,Char)] -> [(Char,Char)] -> [Char] -> Bool
 
+-- takes the fpa, fm, and the state
+iterState :: [(Int,Char)] -> [(Int,Char)] -> [Char] -> Bool
 iterState fpa fm [] = True
 iterState fpa fm all@(t:tasks) = meetsFpa fpa (fromJust $ elemIndex t all , t) && meetsFm fm (fromJust $ elemIndex t all , t) && iterState fpa fm tasks
 
+
 -- takes a list of pairs (mach, task) from forced partial assignment input
 -- and the current pair (mach,task) and returns a Bool
-meetsFpa :: [(Char,Char)] -> (Int,Char) -> Bool
-
-meetsFpa [] pair = False
+meetsFpa :: [(Int,Char)] -> (Int,Char) -> Bool
+meetsFpa [] pair = True
 meetsFpa ((m,t):pairs) (mach,task)
-  | (ord m - 48) == mach && t == task = True
-  | (ord m - 48) /= mach || t /= task = meetsFpa pairs (mach,task)
+  | (m == mach && t /= task) || (m /= mach && t == task) = False
+  | otherwise = meetsFpa pairs (mach,task)
 
 
 -- takes a list of pairs (mach, task) from forbidden machine input
 -- and the current pair (mach,task) and returns a Bool
-meetsFm :: [(Char,Char)] -> (Int, Char) -> Bool
-
+meetsFm :: [(Int,Char)] -> (Int, Char) -> Bool
 meetsFm [] pair = True
 meetsFm ((m,t):pairs) (mach,task)
-  | (ord m - 48) == mach && t == task = False
-  | (ord m - 48) /= mach || t /= task = meetsFpa pairs (mach,task)
+  | m == mach && t == task = False
+  | otherwise = meetsFm pairs (mach,task)
 
 
 -- takes a list of pairs and the state and returns a boolean
 meetsTnt :: [(Char,Char)] -> [Char] -> Bool
-
--- takes a list of pairs of tasks and a tuple of two nearby tasks and returns a boolean
-iterTnt :: [(Char, Char)] -> (Char, Char) -> Bool
-
 meetsTnt list all@(t1 : tasks) = meetsTnt1 list all t1
-
 meetsTnt1 list [] t0 = True
 meetsTnt1 list [t] t0 = iterTnt list (t,t0) && iterTnt list (t0,t)
 meetsTnt1 list (t1 : t2 : tasks) t0 = iterTnt list (t1,t2) && meetsTnt1 list (t2 : tasks) t0
 
 
+-- takes a list of pairs of tasks and a tuple of two nearby tasks and returns a boolean
+iterTnt :: [(Char, Char)] -> (Char, Char) -> Bool
 iterTnt [] pair = True
 iterTnt ((t1, t2):pairs) (t1', t2')
   | t1 == t1' && t2 == t2' = False
@@ -60,11 +54,9 @@ iterTnt ((t1, t2):pairs) (t1', t2')
 
 -- returns the total penalty value so far
 calcPenalty :: Int -> [[Int]] -> [(Char, Char, Int)] -> [Char] -> Int
-
 calcPenalty mach mp tnp state = calcMp mach mp state + calcTnp tnp state
 
 calcMp :: Int -> [[Int]] -> [Char] -> Int
-
 calcMp n mp state
   | state !! n == 'X' = 0
   | otherwise = mp !! n !! (ord (state !! n ) - 65)
@@ -72,9 +64,7 @@ calcMp n mp state
 
 -- takes a list of triplets (tnp) and the state and returns the penalty value
 calcTnp :: [(Char, Char, Int)] -> [Char] -> Int
-
 calcTnp list all@(t1 : tasks) = calcTnp1 list all t1
-
 calcTnp1 list [] t0 = 0
 calcTnp1 list [t] t0 = iterTnp list (t,t0) + iterTnp list (t0,t)
 calcTnp1 list ('X' : tasks) t0 = 0
@@ -82,11 +72,7 @@ calcTnp1 list (a : 'X' : tasks) t0 = 0
 calcTnp1 list (t1 : t2 : tasks) t0 = iterTnp list (t1,t2) + calcTnp1 list (t2 : tasks) t0
 
 iterTnp :: [(Char,Char,Int)] -> (Char,Char) -> Int
-
 iterTnp [] pair = 0
 iterTnp ((t1, t2, p):triplets) (t1', t2')
   | t1 == t1' && t2 == t2' = p
   | otherwise = iterTnp triplets (t1', t2')
-
-
-
