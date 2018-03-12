22643411205d613a97e994221558d5a6e7ab0b89
benstrac <35542776+benstrac@users.noreply.github.com>
2018-03-11 23:10:23 -0600

    Constraints Update
     Changes after first round of full testing

--- src/Constraints.hs
+++ src/Constraints.hs
@@ -39,8 +39,9 @@ meetsFm ((m,t):pairs) (mach,task)
 -- takes a list of pairs and the state and returns a boolean
 meetsTnt :: [(Char,Char)] -> [Char] -> Bool
 meetsTnt list all@(t1 : tasks) = meetsTnt1 list all t1
+
 meetsTnt1 list [] t0 = True
-meetsTnt1 list [t] t0 = iterTnt list (t,t0) && iterTnt list (t0,t)
+meetsTnt1 list [t] t0 = iterTnt list (t,t0) -- && iterTnt list (t0,t)
 meetsTnt1 list (t1 : t2 : tasks) t0 = iterTnt list (t1,t2) && meetsTnt1 list (t2 : tasks) t0
 
 
@@ -53,22 +54,34 @@ iterTnt ((t1, t2):pairs) (t1', t2')
 
 
 -- returns the total penalty value so far
-calcPenalty :: Int -> [[Int]] -> [(Char, Char, Int)] -> [Char] -> Int
-calcPenalty mach mp tnp state = calcMp mach mp state + calcTnp tnp state
+--old single machine calc --calcPenalty :: Int -> [[Int]] -> [(Char, Char, Int)] -> [Char] -> Int
+                          --calcPenalty mach mp tnp state = calcMp mach mp state + calcTnp tnp state
+calcPenalty :: [[Int]] -> [(Char, Char, Int)] -> [Char] -> Int
+calcPenalty mp tnp state = calcMp mp state + calcTnp tnp state
 
-calcMp :: Int -> [[Int]] -> [Char] -> Int
-calcMp n mp state
-  | state !! n == 'X' = 0
-  | otherwise = mp !! n !! (ord (state !! n ) - 65)
+-- old single machine calc -- calcMp :: Int -> [[Int]] -> [Char] -> Int
+                           -- calcMp n mp state
+                           --  | state !! n == 'X' = 0
+                           --  | otherwise = mp !! n !! (ord (state !! n ) - 65)
+-- calculates total Machine Penalty
+-- requires full state, filled with 'X's or an actual state
+calcMp :: [[Int]] -> [Char] -> Int
+calcMp _ [] = 0
+calcMp (x:mp) (y:state) 
+  | y == 'X' && (length state > 0) = calcMp mp state
+  | y == 'X' = 0
+  | otherwise = x !! (ord y - 65) + calcMp mp state
 
 
 -- takes a list of triplets (tnp) and the state and returns the penalty value
 calcTnp :: [(Char, Char, Int)] -> [Char] -> Int
 calcTnp list all@(t1 : tasks) = calcTnp1 list all t1
+
 calcTnp1 list [] t0 = 0
-calcTnp1 list [t] t0 = iterTnp list (t,t0) + iterTnp list (t0,t)
-calcTnp1 list ('X' : tasks) t0 = 0
-calcTnp1 list (a : 'X' : tasks) t0 = 0
+calcTnp1 list [t] t0 = iterTnp list (t,t0) -- + iterTnp list (t0,t)
+calcTnp1 list ('X' : tasks) t0 = calcTnp1 list tasks t0
+-- not sure what this was for
+                -- calcTnp1 list (a : 'X' : tasks) t0 = calcTnp1 list tasks 
 calcTnp1 list (t1 : t2 : tasks) t0 = iterTnp list (t1,t2) + calcTnp1 list (t2 : tasks) t0
 
 iterTnp :: [(Char,Char,Int)] -> (Char,Char) -> Int
@@ -76,3 +89,5 @@ iterTnp [] pair = 0
 iterTnp ((t1, t2, p):triplets) (t1', t2')
   | t1 == t1' && t2 == t2' = p
   | otherwise = iterTnp triplets (t1', t2')
+
+
