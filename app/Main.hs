0c8c4600374c652da97dc40635690762209a7cd5
Tripleo333 <Omieabdelwahab@gmail.com>
2018-03-12 19:05:47 -0600

    main now takes in 2 arguments, more in desc
    main now takes 2 arguments, opens a file using the first item reads it's content and puts it into an array of strings called strings. then takes the second argument and uses it to create a new file and puts the last line read from the previous file into it. There is a commented out line that calls the parser and passes it the array of strings called strings and whatever it returns is what gets added to the file instead. but that can be changed later. Next I will try and catch errors while inputting command line arguments like if they enter more or less than 2 command line arguments and later i will try and error check if the input file doesn't exist

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,15 @@ module Main where
 
 import System.Environment
 import Data.List
+import Control.Monad
+import System.IO
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    [file] -> do
-      x <- readFile file
-      putStr x
-    _ -> putStrLn "Wrong number of arguments"
+  (x:y:args) <- getArgs
+  let sizeArgs = length args
+  handle <- openFile x ReadMode
+  contents <- hGetContents handle
+  let strings = lines contents
+--writeFile y (parser strings)
+  writeFile y (last strings)
