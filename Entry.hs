eea57684930d840e60ace427213f29aac6c4b67e
joell1 <34355199+joell1@users.noreply.github.com>
2018-03-18 21:28:17 -0600

    problem of matching type ie "m24 Fallow doesnt match type Fallow"

--- Entry.hs
+++ Entry.hs
@@ -2,8 +2,8 @@ module Entry where
 import Verify
 import Constraints
 
-data Fallow a = Fallow{ output :: String
-					, line	 :: Int}
+data Fallow = Fallow{ output :: String
+					, li	 :: Int}
 data Loop8 = Loop8{ count	:: Int
 					, out :: String
 					, lin	 :: Int}
@@ -14,11 +14,11 @@ loop8 (m,o,p) = Loop8{ 	count   = m
 
 fallow :: (String,Int)->Fallow
 fallow (str,c) = Fallow{ output = str ++ "?"
-						,line	= c } 
+						,li	= c } 
 					
 checkverifyName :: ([String],Int)->Fallow
-checkverifyName = do 
-					[x,c] <-getargs
+checkverifyName (x,c)= do 
+					
 					if verifyName(head x) == True
 						then do
 							let a = fallow("",c)
@@ -27,8 +27,8 @@ checkverifyName = do
 							let a = fallow("Error while parsing input file",c)
 							return a
 checkverifyBlank :: ([String],Int)->Fallow
-checkverifyBlank = do 
-					[x,c] <-getargs
+checkverifyBlank (x,c)= do 
+					
 					if verifyBlank(x!!c) == True
 						then do
 							let a = fallow("",c+1)
@@ -38,8 +38,8 @@ checkverifyBlank = do
 							return a
 
 checkverifyFPa :: ([String],Int)->Fallow
-checkverifyFPa = do
-					[x,c] <-getargs
+checkverifyFPa (x,c)= do
+					
 					if verifyFPa (x !! c) == True --check forced partial assign
 						then do
 							let a = fallow("",c+1)
@@ -48,30 +48,30 @@ checkverifyFPa = do
 							let a = fallow("Error while parsing input file",c)
 							return a
 loopFPa :: ([String],Fallow)->Fallow
-loopFPa = do
-			[x,a] <-getargs
-			if verifyMachTaskParseError(x !! a line) == True
+loopFPa (x,a)= do
+			
+			if verifyMachTaskParseError(x !! li a) == True
 				then do
-					if verifyMachTaskInvalid(x !! a line) == True
+					if verifyMachTaskInvalid(x !! li a) == True
 						then do
-							ConstraintTup fPa (x !! a line)
-							let a = fallow("",a line + 1)
+							ConstraintTup fPa (getMachTask(x !! li a))
+							let a = fallow("",li a + 1)
 							let a = loopFPa (x,a)
 							return a
 						else do
-							let a = fallow("invalid machine/task",a line)
+							let a = fallow("invalid machine/task",li a)
 							return a
 				else do
-					if verifyBlank (x !! a line) == True	-- check if end of FPa
+					if verifyBlank (x !! li a) == True	-- check if end of FPa
 						then do
-							let a = fallow("",a line + 1)
+							let a = fallow("",li a + 1)
 							return a
 						else do
-							let a = fallow("Error while parsing input file",a line)
+							let a = fallow("Error while parsing input file",li a)
 							return a
 checkverifyFM :: ([String],Int)->Fallow
-checkverifyFM = do
-					[x,c] <-getargs
+checkverifyFM (x,c)= do
+					
 					if verifyFM (x !! c) == True --check forced partial assign
 						then do
 							let a = fallow("",c+1)
@@ -80,30 +80,30 @@ checkverifyFM = do
 							let a = fallow("Error while parsing input file",c)
 							return a
 loopFM :: ([String],Fallow)->Fallow
-loopFM = do
-			[x,a] <-getargs
-			if verifyMachTaskParseError(x !! a line) == True
+loopFM (x,a)= do
+			
+			if verifyMachTaskParseError(x !! li a) == True
 				then do
-					if verifyMachTaskInvalid(x !! a line) == True
+					if verifyMachTaskInvalid(x !! li a) == True
 						then do
-							ConstraintTup fM (x !! a line)
-							let a = fallow("",a line + 1)
+							ConstraintTup fM (getMachTask(x !! li a))
+							let a = fallow("",li a + 1)
 							let a = loopFM (x,a)
 							return a
 						else do
-							let a = fallow("invalid machine/task",a line)
+							let a = fallow("invalid machine/task",li a)
 							return a
 				else do
-					if verifyBlank (x !! a line) == True	-- check if end of FM
+					if verifyBlank (x !! li a) == True	-- check if end of FM
 						then do
-							let a = fallow("",a line + 1)
+							let a = fallow("",li a + 1)
 							return a
 						else do
-							let a = fallow("Error while parsing input file",a line)
+							let a = fallow("Error while parsing input file",li a)
 							return a
 checkverifyTNT :: ([String],Int)->Fallow
-checkverifyTNT = do
-					[x,c] <-getargs
+checkverifyTNT (x,c)= do
+					
 					if verifyTNT (x !! c) == True --check forced partial assign
 						then do
 							let a = fallow("",c+1)
@@ -112,30 +112,30 @@ checkverifyTNT = do
 							let a = fallow("Error while parsing input file",c)
 							return a
 loopTNT :: ([String],Fallow)->Fallow								
-loopTNT = do
-			[x,a] <-getargs
-			if verifyTaskTaskParseError(x !! a line) == True
+loopTNT (x,a)= do
+			
+			if verifyTaskTaskParseError(x !! li a) == True
 				then do
-					if verifyTaskTaskInvalid(x !! a line) == True
+					if verifyTaskTaskInvalid(x !! li a) == True
 						then do
-							ConstraintTup tNt (x !! a line)
-							let a = fallow("",a line + 1)
+							ConstraintTup tNt (getTaskTask(x !! li a))
+							let a = fallow("",li a + 1)
 							let a = loopFM (x,a)
 							return a
 						else do
-							let a = fallow("invalid machine/task",a line)
+							let a = fallow("invalid machine/task",li a)
 							return a
 				else do
-					if verifyBlank (x !! a line) == True	-- check if end of FM
+					if verifyBlank (x !! li a) == True	-- check if end of FM
 						then do
-							let a = fallow("",a line + 1)
+							let a = fallow("",li a + 1)
 							return a
 						else do
-							let a = fallow("Error while parsing input file",a line)
+							let a = fallow("Error while parsing input file",li a)
 							return a
 checkverifyMP :: ([String],Int)->Fallow
-checkverifyMP = do
-					[x,c] <-getargs
+checkverifyMP (x,c)= do
+					
 					if verifyMP (x !! c) == True --check forced partial assign
 						then do
 							let a = fallow("",c+1)
@@ -144,54 +144,53 @@ checkverifyMP = do
 							let a = fallow("Error while parsing input file",c)
 							return a
 loopMP :: ([String],Fallow,Int)->Loop8
-loopMP = do 
-			[x,a,n] <-getargs
+loopMP (x,a,n)= do 
 			if n == 0
 				then do
-					if verifyBlank (x !! a line) == True	-- check if end of FM
+					if verifyBlank (x !! li a) == True	-- check if end of FM
 						then do
-							let a = fallow("",a line+1)
-							let b = loop8(n,a output,a line)
+							let a = fallow("",li a+1)
+							let b = loop8(n,output a,li a)
 							return b
 						else do
-							let a = fallow("",a line)
-							let b = loop8(n,a output,a line)
+							let a = fallow("",li a)
+							let b = loop8(n,output a,li a)
 							return b
 				else do
-					if verifyMachPenaltiesParseError (x !! a line) == True
+					if verifyMachPenaltiesParseError (x !! li a) == True
 						then do
-							if verifyMachPenaltiesPenaltyError (x !! a line) == True
+							if verifyMachPenaltiesPenaltyError (x !! li a) == True
 								then do
-									if verifyMachPenaltiesInvalidPenalty (x !! a line) == True
+									if verifyMachPenaltiesInvalidPenalty (x !! li a) == True
 										then do
-											ConstraintTup mP (getMachPenaltyLine(x !! a line))
-											let a = fallow("",a line+1)
-											let b = loop8(n-1,a output,a line)
-											let b = loopMP (x,a,b count)
+											ConstraintTup mP (getMachPenaltyLine(x !! li a))
+											let a = fallow("",li a+1)
+											let b = loop8(n-1,output a,li a)
+											let b = loopMP (x,a,count b)
 											return b
 										else do
-											let a = fallow("invalid penalty",a line)
-											let b = loop8(n,a output,a line)
+											let a = fallow("invalid penalty",li a)
+											let b = loop8(n,output a,li a)
 											return b
 								else do
-									let a = fallow("machine penalty error",a line)
-									let b = loop8(n,a output,a line)
+									let a = fallow("machine penalty error",li a)
+									let b = loop8(n,output a,li a)
 									return b
 								
 						else do
-							if verifyBlank (x !! c) == True
+							if verifyBlank (x !! li a) == True
 								then do
-									let a = fallow("machine penalty error",a line)
-									let b = loop8(n,a output,a line)
+									let a = fallow("machine penalty error",li a)
+									let b = loop8(n,output a,li a)
 									return b
 								
 								else do
-									let a = fallow("Error while parsing input file",a line)
-									let b = loop8(n,a output,a line)
+									let a = a.("Error while parsing input file",li a)
+									let b = loop8(n,output a,li a)
 									return b
 checkverifyTNP :: ([String],Int)->Fallow
-checkverifyTNP = do
-					[x,c] <-getargs
+checkverifyTNP (x,c)= do
+					
 					if verifyTNP (x !! c) == True --check forced partial assign
 						then do
 							let a = fallow("",c+1)
@@ -200,48 +199,50 @@ checkverifyTNP = do
 							let a = fallow("Error while parsing input file",c)
 							return a
 
-loopTNP :: ([String],Int) -> ([String],Int)		--similar to previous loops but with TNP
-loopTNP = do
-		[x,a] <-getargs
-		if verifyTooNearPenaltiesParseError(x !! a line) == True
-			then do
-				if verifyTooNearPenaltiesInvalidTask (x !! a line) == True
-					then do
-						if verifyTooNearPenaltiesInvalidPenalty (x !! a line) == True
-							then do
-								ConstraintTup fM (getTaskTaskPenalty(x !! a line))
-								let a = fallow("",a line + 1)
-								let a = loopFM (x,a)
-								return a
-							else do
-								let a = fallow("invalid penalty",a line)
-								return a
-					else do
-						let a = fallow("invalid task",a line)
-						return a
-				else do
-					if verifyBlank (x !! a line) == True	-- check if end of FM
+loopTNP :: ([String],Fallow) -> Fallow		--similar to previous loops but with TNP
+loopTNP (x,a)= do
+			let m = (length li)-1
+			let q = m+1
+			if verifyTooNearPenaltiesParseError(x !! m) == True
+				then do
+					if verifyTooNearPenaltiesInvalidTask (x !! m) == True
 						then do
-							let a = fallow("",a line + 1)
-							return a
+							if verifyTooNearPenaltiesInvalidPenalty (x !! m) == True
+								then do
+									--ConstraintTup fM (getTaskTaskPenalty(x !! m)
+									let a1 = fallow("",q)
+									let a2 = loopFM (x,a1)
+									return a2
+								else do
+									let a1 = fallow("invalid penalty",m)
+									return a1
 						else do
-							let a = fallow("Error while parsing input file",a line)
-							return a						
+							let a1 = fallow("invalid task",m)
+							return a1
+					else do
+					
+						if verifyBlank (x !! m) == True	-- check if end of FM
+							then do
+								let a1 = fallow("",m)
+								return a1
+							else do
+								let a = a.("Error while parsing input file",m)
+								return a						
 entry :: [String]->String
-entry = do
-		[x] <-getargs
+entry x = do
 		let c = 0	--counter to track line of list
-		let a = fallow("",c)
-		let a = checkverifyName(x,a line)
-		let a = checkverifyBlank(x,a line)
-		let a = checkverifyFPa(x,a line)
-		let a = loopFPa(x,a)
-		let a = checkverifyFM(x,a line)
-		let a = loopFM(x,a)
-		let a = checkverifyTNT(x,a line)
-		let a = loopTNT(x,a)
-		let a = checkverifyMP(x,a line)
-		let b = loopMP(x,a,8)
-		let a = checkverifyTNP(x,b fall line)
-		let a = loopTNP(x,a)
-		x+7	
+		let a1 = [].fallow("",c)
+		let a2 = checkverifyName(x,li a1)
+		let a3 = checkverifyBlank(x,li a2)
+		let a4 = checkverifyFPa(x,li a3)
+		let a5 = loopFPa(x,a4)
+		let a6 = checkverifyFM(x,li a5)
+		let a7 = loopFM(x,a6)
+		let a8 = checkverifyTNT(x,li a7)
+		let a9 = loopTNT(x,a8)
+		let a10 = checkverifyMP(x,li a9)
+		let b = loopMP(x,a10,8)
+		let a11 = checkverifyTNP(x,lin b)
+		let a12 = loopTNP(x,a11)
+		let d =	output a12
+		return d
